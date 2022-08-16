97cd9b7219b28b2644346c4b620f7d06336f112c
Mario Miranda <mario_chan15@hotmail.com>
2022-08-16 13:34:09 -0400

    Adding security / disabling csrf / adding initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+insert 
+into
+    tb_parking_spot
+    (apartment, block, brand_car, color_car, license_plate_car, model_car, parking_spot_number, registration_date, responsible_name, id) 
+values
+    ('201', '3', 'Hyundai', 'Grey', 'E98YLX', 'Tucson', '153', TIMESTAMP WITH TIME ZONE '2022-08-16T11:33:07.12345Z', 'Mario Miranda', 'f4d3c82f-58c8-47d7-97c5-f7cb905f7039');
+    
+insert 
+into
+    tb_parking_spot
+    (apartment, block, brand_car, color_car, license_plate_car, model_car, parking_spot_number, registration_date, responsible_name, id) 
+values
+    ('201', '3', 'Ford', 'Grey', 'F09YLX', 'Mustang', '152', TIMESTAMP WITH TIME ZONE '2022-08-15T11:33:07.12345Z', 'Cristina Chan', 'de24f5ee-46a2-4ddf-ba74-1c511bcfd2e1');
